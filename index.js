d2cca09310c1f36cc4b3f598e571bc8321b62365
Haroon <mharoonbutt13@gmail.com>
2023-02-26 19:38:05 +0000

    all classes tests are passed

--- index.js
+++ index.js
@@ -9,7 +9,77 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
+const { title } = require("process");
+const { choices } = require("yargs");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+// const manager = new Manager();
+console.log("Please enter team managers details:");
+
+function askForGeneralDetails() {
+    return inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "title",
+          message: "Enter the name",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Enter the Id",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter the email",
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "Enter Office number",
+        }
+      ])
+      .then(answers => {
+        const manager = new Manager(answers.title, answers.id, answers.email, answers.officeNumber);
+        console.log(manager)
+        askForOptions();
+      });
+    }
+    askForGeneralDetails();
+
+function quit() {
+    console.log("\nGoodbye!");
+    process.exit(0);
+  }
+
+function askForOptions() {
+    inquirer
+      .prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "Please select an option.",
+            choices: [
+                "Add an Engineer",
+                "Add an Intern",
+                "Quit",
+            ],
+        },
+      ])
+      .then(val => {
+        if (val.choice === "Add an Engineer") {
+          console.log("Eng");
+          askForGeneralDetails()
+        }
+        else if(val.choice == "Add an Intern") {
+            console.log("Intern");
+        }
+        else{
+            console.log("Quit");
+            quit();
+        }
+      });
+  }
 
